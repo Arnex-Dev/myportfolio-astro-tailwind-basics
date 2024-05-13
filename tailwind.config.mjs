02297c2a155516de7665e094e71b7ae418122b56
ArnexDev <mrankex.dev@gmail.com>
2024-05-13 17:01:27 -0600

    Fix css colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'glow': '0 0 5px rgba(139, 92, 246, 1), 0 0 25px rgba(139, 92, 246, 1)',
+			  },
+		},
 	},
 	plugins: [],
 }
